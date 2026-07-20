5a46f453b35ee19ff45f680ab871bb04706544ff
Ryan <Woodedcode@gmail.com>
2026-07-20 11:08:20 -0500

    Implement background movement on mouse move
    Added background movement functionality to enhance user interaction.

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 let currentPosition = 0
+const scaleFactor = 1 / 20;
 
 
 
@@ -15,3 +16,16 @@ const cardsRow = document.querySelector(".popular-hits__cards");
 currentPosition -= 250;
 cardsRow.style.transform = "translateX(" + currentPosition + "px)";
 })
+
+// BACKGROUND MOVEMENT
+function moveBackground(event) {
+    const shapes = document.querySelectorAll(".shape");
+    const x = event.clientX * scaleFactor;
+    const y = event.clientY * scaleFactor;
+
+    for (let i = 0; i < shapes.length; i++) {
+        const isOdd = i % 2 !== 0;
+        const boolInt = isOdd ? -1 : 1;
+        shapes[i].style.transform = `translate(${x * boolInt}px,${y * boolInt}px)`
+    }
+}
